6ef333862d679fcb44a3a25d561ff09dd2a32a43
rickyanggoro <ricky.mdksvc2233@gmail.com>
2024-10-29 15:50:45 +0700

    Add files via upload
    kita sedang mencoba upload sebuah syintx

---
+++ latihan1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan1</title>
+</head>
+<body>
+   <?php
+   
+
+   echo "elamat Sore Guys.....! </br>
+   <b><font color=red> Nama : Ricky Anggoro</font> </br>
+   <b>NPM : 23753113 </b></br>
+  <font color=blue> <i>Prodi : Manajemen Informatika </i></br> </font>
+   <i>Jurusan : Teknologi Informasi </i></br>" ;
+   
+   
+   ?> 
+</body>
+</html>
